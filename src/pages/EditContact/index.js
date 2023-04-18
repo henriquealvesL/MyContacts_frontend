8f2e641ef6ee4d4c7ab7d72fb0eb46dc33469ef9
henrique <henriqueadlima@hotmail.com>
2023-04-18 18:23:42 -0300

    Implementing SafeAsyncAction and isMounted hooks
    These hooks were created to prevent bugs on edit page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,12 +1,13 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import { useParams, useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 import ContactsService from "../../services/ContactsService";
+import useSafeAsyncAction from "../../hooks/useSafeAsyncAction";
 import toast from "../../utils/toast";
 
-import PageHeader from "../../components/PageHeader";
 import ContactForm from "../../components/ContactForm";
 import Loader from "../../components/Loader";
+import PageHeader from "../../components/PageHeader";
 
 export default function EditContact() {
   const [isLoading, setIsLoading] = useState(true);
@@ -17,19 +18,25 @@ export default function EditContact() {
   const { id } = useParams();
   const history = useHistory();
 
+  const safeAsyncAction = useSafeAsyncAction();
+
   const loadContact = useCallback(async () => {
     try {
       const contactData = await ContactsService.getContactById(id);
 
       contactFormRef.current.setFieldsValues(contactData);
 
-      setIsLoading(false);
-      setContactName(contactData.name);
+      safeAsyncAction(() => {
+        setIsLoading(false);
+        setContactName(contactData.name);
+      });
     } catch (error) {
-      history.push("/");
-      toast({ type: "danger", text: "Contato não encontrado" });
+      safeAsyncAction(() => {
+        history.push("/");
+        toast({ type: "danger", text: "Contato não encontrado" });
+      });
     }
-  }, [id, history]);
+  }, [id, history, safeAsyncAction]);
 
   useEffect(() => {
     loadContact();
