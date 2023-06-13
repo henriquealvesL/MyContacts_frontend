3b8d79e2b8520c5db741680441fd3c73687644ca
henrique <henriqueadlima@hotmail.com>
2023-06-13 18:03:33 -0300

    refactor: changing the way to define default props

--- src/components/ReactPortal/index.js
+++ src/components/ReactPortal/index.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
-export default function ReactPortal({ containerId, children }) {
+export default function ReactPortal({ containerId = "portal-root", children }) {
   let container = document.getElementById(containerId);
 
   if (!container) {
